4c0471d87ae98d565f222e3b5eb2601c6ee3e021
jesserussell <jesserussell411@gmail.com>
2021-10-08 18:32:33 -0600

    fetch test

--- marketplace.jsx
+++ marketplace.jsx
@@ -1,8 +1,16 @@
 const reactContainer = document.getElementById("react_container");
-const {useState} = React;
+const {useState, useEffect} = React;
 
 function Main(){
     const [testState, setTestState] = useState(6);
+
+    useEffect(async () => {
+        const result = await fetch("gethello.php?who=world", {
+            method:"GET",
+        });
+        setTestState(await result.text());
+    });
+
     return <p>main test {3+testState}<button onClick={() => setTestState(8)}>test set state</button></p>;
 }
 
